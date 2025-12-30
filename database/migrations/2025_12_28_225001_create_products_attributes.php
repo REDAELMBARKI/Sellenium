f644850cbcaa99e338d815970e2c082076e492ac
reda elm <elmreda05@gmail.com>
2025-12-30 21:58:29 +0100

    before fixing front ui to remove niches consept and make evrything fields suggestions based on the selected category instead of niche making it hybrid for all kind of niches and products

--- database/migrations/2025_12_28_225001_create_products_attributes.php
+++ database/migrations/2025_12_28_225001_create_products_attributes.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('attributes', function (Blueprint $table) {
+        Schema::create('products_attributes', function (Blueprint $table) {
         $table->id();
         $table->string('owner_type');   // product | variant
         $table->unsignedBigInteger('owner_id'); // product_id / variant_id
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('attributes');
+        Schema::dropIfExists('products_attributes');
     }
 };
