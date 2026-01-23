ee6fd5f3498bcf3f5ca068a99439bf493ac67457
reda elm <elmreda05@gmail.com>
2026-01-23 22:58:29 +0100

    attriibutes migrations done -> fixing AttibutesController (crud)

---
+++ database/migrations/2026_01_23_215638_create_attributes_values.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('attribute_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('attribute_id')->constrained()->cascadeOnDelete();
+            $table->string('value');         // Red, XL, Cotton
+            $table->json('meta')->nullable(); // hex, extra info
+            $table->integer('sort_order')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attributes_values');
+    }
+};
