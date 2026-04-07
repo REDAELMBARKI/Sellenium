7df47eafa935775f34f9f9eba15360b13f071002
soufiane <soufianechbicheb1997@gmail.com>
2026-04-07 01:04:00 +0100

    migration invitations table + googleAuth - googleDrive - servive.php - package-lock

--- config/googleDrive.php
+++ config/googleDrive.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'application_name' => env('APP_NAME', 'MyEcomApp'),
-    'redirect_uri' => 'http://localhost:8000/sheetAuth/google/callback' ,
-    'oauth_drive_credentials_path' => storage_path('app/google/googleDrive-credentials.json'),
+    // 'application_name' => env('APP_NAME', 'MyEcomApp'),
+    // 'redirect_uri' => 'http://localhost:8000/sheetAuth/google/callback' ,
+    // 'oauth_drive_credentials_path' => storage_path('app/google/googleDrive-credentials.json'),
 ];
